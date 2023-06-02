c58a222260a94f7c3f1c35328f3286ada6b3fa8f
daniyalwaseem11 <99401423+DaniyalWaseem12@users.noreply.github.com>
2023-06-02 15:21:15 +0500

    Update

--- app.vue
+++ app.vue
@@ -9,28 +9,22 @@
     <demolayout />
     <Pricing />
     <NewsBlog />
-    <newfooter />
+    <NewFooter />
   </div>
 </template>
 
 <script setup>
+import "~/assets/js/vendor/modernizr-3.6.0.min.js";
+import "~/assets/js/vendor/jquery-1.12.4.min.js";
+import "~/assets/js/bootstrap.min.js";
+import "~/assets/js/popper.min.js";
+import "~/assets/js/slick.min.js";
+import "~/assets/js/slick-menu.min.js";
+import "~/assets/js/jquery.counterup.min.js";
+import "~/assets/js/jquery.magnific-popup.min.js";
+import "~/assets/js/jquery.nav.js";
+import "~/assets/js/main.js";
 useHead({
-  title: "Daniyal",
-  link: [
-    {
-      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
-      rel: "stylesheet",
-      integrity: "sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx",
-      crossorigin: "anonymous"
-    },
-  ],
-  script: [
-    {
-      src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
-      integrity: "sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa",
-      crossorigin: "anonymous"
-    },
-  ]
-})
+  title: "Test",
+});
 </script>
-
