c6561330ae17a8b5d48e461bb0fa1fe05706d9fe
daniyalwaseem11 <99401423+DaniyalWaseem12@users.noreply.github.com>
2023-06-06 04:15:12 +0500

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["~/assets/css/main.css"],
+  css: [
+    "~/assets/css/main.css",
+    "~/assets/css/style.css",
+    "~/assets/css/all.css",
+    "~/assets/css/bootstrap.min.css",
+    "~/assets/css/default.css",
+    "~/assets/css/fontawesome-all.min.css",
+    "~/assets/css/magnific-popup.css",
+    "~/assets/css/responsive.css",
+    "~/assets/css/slick-menu.css",
+    "~/assets/css/slick.css",
+    "~/assets/css/slick.css",
+  ],
 
   components: true,
   alias: {
