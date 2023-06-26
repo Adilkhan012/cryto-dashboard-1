f9ab6fc418e8e8981d9108ffd2a42c6dae36bc6d
daniyalwaseem11 <99401423+DaniyalWaseem12@users.noreply.github.com>
2023-06-26 08:04:50 +0500

    Update

--- components/demolayout.vue
+++ components/demolayout.vue
@@ -382,6 +382,8 @@
 import SwiperClass, { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 
+import "swiper/css";
+// import "swiper/css/pagination";
 import { gsap } from "gsap";
 export default {
   setup() {
